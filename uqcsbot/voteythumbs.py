a0f8f75d5e279e1048e3c6f25970bd96aa3b959b
James Dearlove <jamesdearlove@outlook.com>
2021-06-29 23:44:43 +1000

    Added thumbsright to voteythumbs

--- uqcsbot/voteythumbs.py
+++ uqcsbot/voteythumbs.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 class VoteyThumbs(commands.Cog):
@@ -7,9 +8,13 @@ class VoteyThumbs(commands.Cog):
     @commands.command()
     async def voteythumbs(self, ctx: commands.Context):
         """ Starts a 👍 👎 vote. """
-        # await ctx.send(text)
+        
         await ctx.message.add_reaction("👍")
         await ctx.message.add_reaction("👎")
+
+        # thumbsright is a custom server emoji, get the Discord emoji string for it.
+        thumbsright = discord.utils.get(self.bot.emojis, name="thumbsright")
+        await ctx.message.add_reaction(str(thumbsright))
 
 def setup(bot: commands.Bot):
     bot.add_cog(VoteyThumbs(bot))
