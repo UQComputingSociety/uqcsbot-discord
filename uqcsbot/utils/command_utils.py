6ee92e362213b5881b7dd690bb5af8f4fec6f526
TomCranitch <tom@cranitch.com.au>
2021-07-12 20:11:30 +1000

    Cleanup

--- uqcsbot/utils/command_utils.py
+++ uqcsbot/utils/command_utils.py
@@ -5,25 +5,12 @@ from discord.ext import commands
 
 LOADING_REACTS = ["⏰", "🕰️", "⏲️", "🕖", "🕔", "🕥"]
 
-
-"""
-async def loading_status(command_fn: Callable):
-    @wraps(command_fn)
-    async def wrapper(self, ctx: commands.Context):
-        react = choice(LOADING_REACTS)
-        await ctx.message.add_reaction(react)
-        res = await command_fn(self, ctx)
-        await ctx.message.remove_reaction(react)
-        return res
-    return wrapper
-"""
-
-def loading_status(coro):
-    @wraps(coro)  # Important to preserve name because `command` uses it
+def loading_status(command_fn: Callable):
+    @wraps(command_fn)  # Important to preserve name because `command` uses it
     async def wrapper(self, ctx: commands.Context):
         react = choice(LOADING_REACTS)
         reaction = await ctx.message.add_reaction(react)
-        res = await coro(self, ctx)
+        res = await command_fn(self, ctx)
         await ctx.message.remove_reaction(react, ctx.bot.user)
         return res
     return wrapper
