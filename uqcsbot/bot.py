15a914280ac22868c6db6f2f8981608ec36a53cb
James Dearlove <jamesdearlove@outlook.com>
2021-07-21 00:11:32 +1000

    Start of database integration
    Postgres is now part of Docker.
    Database is setup from models.

--- uqcsbot/bot.py
+++ uqcsbot/bot.py
@@ -2,6 +2,8 @@ import logging
 import discord
 from discord.ext import commands
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker
 
 class UQCSBot(commands.Bot):
     """ An extended bot client to add extra functionality. """
@@ -13,6 +15,10 @@ class UQCSBot(commands.Bot):
     def schedule_task(self, func, *args, **kwargs):
         """ Schedule a function to be run at a later time. A wrapper for apscheduler add_job. """
         self._scheduler.add_job(func, *args, **kwargs)
+
+    def set_db_engine(self, db_engine: Engine):
+        """ Creates a sessionmaker from the provided database engine which can be called from commands. """
+        self.create_db_session = sessionmaker(bind=db_engine)
 
     async def on_ready(self):
         """ Once the bot is loaded and has connected, run these commands first. """
