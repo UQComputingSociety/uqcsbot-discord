39883cae79b3e01d64f39c7da978467cf4b963e3
TomCranitch <tom@cranitch.com.au>
2021-07-21 13:19:56 +1000

    Fix loading wrapper

--- uqcsbot/utils/command_utils.py
+++ uqcsbot/utils/command_utils.py
@@ -1,16 +1,19 @@
 from random import choice
 from functools import wraps
-from typing import Callable, List
+from typing import Callable
 from discord.ext import commands
 
 LOADING_REACTS = ["⏰", "🕰️", "⏲️", "🕖", "🕔", "🕥"]
 
 def loading_status(command_fn: Callable):
     @wraps(command_fn)  # Important to preserve name because `command` uses it
-    async def wrapper(self, ctx: commands.Context):
+    async def wrapper(self, ctx: commands.Context, *args):
+        if ctx.message is None or ctx.bot is None:
+            return
+
         react = choice(LOADING_REACTS)
         reaction = await ctx.message.add_reaction(react)
-        res = await command_fn(self, ctx)
+        res = await command_fn(self, ctx, *args)
         await ctx.message.remove_reaction(react, ctx.bot.user)
         return res
     return wrapper
