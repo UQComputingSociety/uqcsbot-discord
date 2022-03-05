df18ee95d9ee4b4ce17af899ea3c81f590c6eba5
James Dearlove <39483549+JamesDearlove@users.noreply.github.com>
2022-03-05 18:11:18 +1000

    Azure Migration with Web Server (#39)
    * Adds webserver using aiohttp
    
    * Updates to deploy to Azure
    
    * Web server comment

---
+++ uqcsbot/web.py
@@ -0,0 +1,29 @@
+import asyncio
+from discord.ext import commands
+from aiohttp import web
+from uqcsbot.bot import UQCSBot
+
+class Web(commands.Cog):
+    def __init__(self, bot: UQCSBot):
+        self.bot = bot
+        self.bot.loop.create_task(self.web_server())
+
+    # Web server binds to port 8080. This is a basic template to ensure
+    # that Azure has something for a health check.
+    async def web_server(self):
+        def handle(request):
+            return web.Response(text="UQCSbot is running")
+
+        app = web.Application()
+        app.router.add_get('/', handle)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        self.site = web.TCPSite(runner, '0.0.0.0', 8080)
+        await self.bot.wait_until_ready()
+        await self.site.start()
+
+    def __unload(self):
+        asyncio.ensure_future(self.site.stop())
+
+def setup(bot: UQCSBot):
+    bot.add_cog(Web(bot))
