1ca8a328adb7b4f958627365bdd9012678f0fcfc
shirei220 <110725093+shirei220@users.noreply.github.com>
2023-03-18 15:02:54 +1000

    added ansi colours to cat

--- uqcsbot/cat.py
+++ uqcsbot/cat.py
@@ -11,12 +11,21 @@ class Cat(commands.Cog):
         """
         Displays the moss cat. Brings torture to CSSE2310 students.
         """
-        cat = "\n".join(("```",
-                    "         __..--''``\\--....___   _..,_            ",
-                    "     _.-'    .-/\";  `        ``<._  ``-+'~=.     ",
-                    " _.-' _..--.'_    \\                    `(^) )    ",
-                    "((..-'    (< _     ;_..__               ; `'   fL",
-                    "           `-._,_)'      ``--...____..-'         ```"))
+
+        #ansi colour codes
+        pink = "\u001b[0;35m"
+        red = "\u001b[0;31m"
+        yellow = "\u001b[0;33m"
+        green = "\u001b[0;32m"
+        cyan = "\u001b[0;36m"
+        blue = "\u001b[0;34m"
+
+        cat = "\n".join(("```ansi",
+                    f"{pink}       {red}  __..--{yellow}''``\\-{green}-....___{cyan}   _..,_{blue}            ",
+                    f"{pink}     _.{red}-'    .-{yellow}/\";  `{green}        {cyan}``<._  `{blue}`-+'~=.     ",
+                    f"{pink} _.-' _{red}..--.'_ {yellow}   \\  {green}        {cyan}        {blue}  `(^) )    ",
+                    f"{pink}((..-' {red}   (< _ {yellow}    ;_.{green}.__     {cyan}        {blue}  ; `'   fL",
+                    f"{pink}       {red}    `-._{yellow},_)'   {green}   ``--.{cyan}..____..{blue}-'         ```"))
         await interaction.response.send_message(cat)
 
 async def setup(bot: commands.Bot):
