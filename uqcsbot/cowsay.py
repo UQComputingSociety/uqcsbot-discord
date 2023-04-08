70f7fbbf00fb4905420e30501ad447d3fb87bbd1
Lachlan Cox <lcox74@outlook.com>
2023-04-08 13:29:31 +1000

    check valid message length

--- uqcsbot/cowsay.py
+++ uqcsbot/cowsay.py
@@ -43,6 +43,11 @@ class Cowsay(commands.Cog):
         Returns a cow or tux saying the given message.
         """
 
+        # Check message length, if invalid send moo!
+        if len(message) == 0 or len(message) > 2000:
+            await interaction.response.send_message("moo!")
+            return
+
         ascii_art = self.draw_cow(mood, tongue) if not tux else self.draw_tux(mood)
         await interaction.response.send_message(f"```{self.construct_say_bubble(message)}{ascii_art}```")
 
