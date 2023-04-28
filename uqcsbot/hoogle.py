38636bbadfa9534dbd6228c234a9051a81f0bec6
shirei220 <110725093+shirei220@users.noreply.github.com>
2023-04-28 12:31:14 +1000

    changed hoogle to use embeds for cleaner link formatting

--- uqcsbot/hoogle.py
+++ uqcsbot/hoogle.py
@@ -16,12 +16,17 @@ class Hoogle(commands.Cog):
     def get_endpoint(self, type_sig: str) -> str:
         unescaped = html.unescape(type_sig)
         return "https://www.haskell.org/hoogle/?mode=json&hoogle=" + unescaped + "&start=0&count=10"
+    
+    def get_hoogle_page(self, type_sig: str) -> str:
+        return f"https://hoogle.haskell.org/?hoogle={html.unescape(type_sig)}"
 
     def pretty_hoogle_result(self, result: dict) -> str:
         url = result['url']
-        type_sig = re.sub('<[^<]+?>', '', result['item']).replace('&gt;', '>')
+        type_sig = re.sub('<[^<]+?>', '', result['item']).replace('&gt;', '>').replace('&#39;', "'")
 
-        return f"`{type_sig}` <{url}|link>"
+        return f"`{type_sig}` [link]({url})"
+    
+    
     
     @app_commands.command()
     @app_commands.describe(search="Function name or type signature to search for")
@@ -30,6 +35,7 @@ class Hoogle(commands.Cog):
         Queries the Hoogle Haskell API search engine, searching Haskell libraries by either function name 
         or approximate type signature. Retrieves up to 10 results.
         """
+        # Note: verbose feature not implemented as it may exceed discord's message length limit
         await interaction.response.defer(thinking=True)
 
         endpoint_url = self.get_endpoint(search)
@@ -45,8 +51,17 @@ class Hoogle(commands.Cog):
             await interaction.edit_original_response(content="No results found")
             return
         
+
         message = "\n".join(self.pretty_hoogle_result(result) for result in results)
-        await interaction.edit_original_response(content=message)
+
+        #test out embed
+        embed = discord.Embed(
+            title = search,
+            url = self.get_hoogle_page(search),
+            description = message,
+            color = 0x800080
+        )
+        await interaction.edit_original_response(embed=embed)
     
 async def setup(bot: commands.Bot):
     await bot.add_cog(Hoogle(bot))
