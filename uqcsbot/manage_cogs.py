0572ef3de5c8ce8506cd1f688a593f7d813613d8
Isaac Beh <isaacwbeh@gmail.com>
2023-06-30 13:06:18 +1000

    Formatted woth black

--- uqcsbot/manage_cogs.py
+++ uqcsbot/manage_cogs.py
@@ -4,18 +4,25 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+
 class ManageCogs(commands.Cog):
     """
     Note that most of these commands can make the bot load files to execute. Care should be made to ensure only entrusted users have access.
     """
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
     @app_commands.command(name="managecogs")
     @app_commands.describe(
-        cog="The cog (i.e. python file) to try to unload. Use python package notation, so no suffix of \".py\" and \".\" between folders: e.g. \"manage_cogs\".",
+        cog='The cog (i.e. python file) to try to unload. Use python package notation, so no suffix of ".py" and "." between folders: e.g. "manage_cogs".',
     )
-    async def manage_cogs(self, interaction: discord.Interaction, action: Literal["load", "unload", "reload"], cog: str):
+    async def manage_cogs(
+        self,
+        interaction: discord.Interaction,
+        action: Literal["load", "unload", "reload"],
+        cog: str,
+    ):
         """
         Trys to unload a cog (i.e. python file).
         Note that most of these commands can make the bot load files to execute. Care should be made to ensure only entrusted users have access.
@@ -30,11 +37,13 @@ class ManageCogs(commands.Cog):
                     await self.bot.reload_extension(f"uqcsbot.{cog}")
         except Exception as error:
             # Many errors can be caught during loading/unloading/reloading the bot, so it would be painful to separate by exception type
-            await interaction.response.send_message(f"Error occured {action}ing {cog}: {error}")
+            await interaction.response.send_message(
+                f"Error occured {action}ing {cog}: {error}"
+            )
             return
         await interaction.response.send_message(f"Successfully {action}ed {cog}")
         await self.bot.tree.sync()
 
-        
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(ManageCogs(bot))
