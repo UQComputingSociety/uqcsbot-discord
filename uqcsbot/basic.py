8591ab789a00353cfbdec7e9768bd36e718dd06d
Isaac Beh <isaacwbeh@gmail.com>
2023-07-02 15:18:35 +1000

    Made repo commands use embeds

--- uqcsbot/basic.py
+++ uqcsbot/basic.py
@@ -1,25 +1,38 @@
-from typing import List
+from typing import Tuple, Dict, Literal
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-UQCS_REPO_URL = "https://github.com/UQComputingSociety/"
+UQCS_EMOJI_NAME = "uqcs"
 
-REPOS = {
-    "cpg": ("cpg", "Resources for the UQCS competitive programming group"),
-    "conduct": (
+UQCS_REPO_URL = "https://github.com/UQComputingSociety/"
+RepoNameType = Literal[
+    "CPG",
+    "Conduct",
+    "Constitution",
+    "Cookbook",
+    "Design",
+    "Events",
+    "Minutes",
+    "Signup",
+    "UQCSbot",
+    "Website",
+]
+REPOS: Dict[RepoNameType, Tuple[str, str]] = {
+    "CPG": ("cpg", "Resources for the UQCS competitive programming group"),
+    "Conduct": (
         "code-of-conduct",
         "The UQCS Code of Conduct to be followed by all community members",
     ),
-    "constitution": ("constitution", "All the business details"),
-    "cookbook": ("cookbook", "A cookbook of recipes contributed by UQCS members"),
-    "design": ("design", "All UQCS design assets"),
-    "events": ("events", "A repository for events and talk materials"),
-    "minutes": ("minutes", "Minutes from UQCS general meetings"),
-    "signup": ("signup", "The UQCS membership signup system"),
-    "uqcsbot": ("uqcsbot-discord", "Our friendly little Discord bot"),
-    "website": ("website", "The UQ Computing Society website"),
+    "Constitution": ("constitution", "All the business details"),
+    "Cookbook": ("cookbook", "A cookbook of recipes contributed by UQCS members"),
+    "Design": ("design", "All UQCS design assets"),
+    "Events": ("events", "A repository for events and talk materials"),
+    "Minutes": ("minutes", "Minutes from UQCS general meetings"),
+    "Signup": ("signup", "The UQCS membership signup system"),
+    "UQCSbot": ("uqcsbot-discord", "Our friendly little Discord bot"),
+    "Website": ("website", "The UQ Computing Society website"),
 }
 
 
@@ -66,57 +79,43 @@ class Basic(commands.Cog):
             "UQCS Code of Conduct: https://uqcs.org/code-of-conduct"
         )
 
-    def find_repo(self, repo: str) -> str:
+    def add_repo_to_embed(self, embed: discord.Embed, repo: str):
         """
-        Finds a specific repo from the REPO dict and constructs a single string for it.
-        :param repo: name of the repo to find.
-        :return: single string containing the info for the given repo.
+        Finds a specific repo from the REPO dict and adds it to the given embed as a field.
         """
-        if repo not in REPOS.keys():
-            return f'> Unrecognised repo "{repo}"\n'
-        else:
-            return f"> {UQCS_REPO_URL + REPOS[repo][0]}: {REPOS[repo][1]}\n"
+        if repo not in REPOS:
+            embed.add_field(
+                name=f'Unrecognised repo "{repo}"',
+                value="Try using `/repo list` to see all UQCS repositories.",
+            )
+            return
 
-    def format_repo_message(self, repos: List[str]) -> str:
-        """
-        Takes a list of repo names and matches them to REPOS keys, constructing a message from the
-        relevant repo information.
-        :param repos: list of strings of repo names
-        :return: a single string with a formatted message containing repo info for the given names
-        """
-        repo_strings: List[str] = []
-        for potential_repo in repos:
-            repo_strings.append(self.find_repo(potential_repo))
-        return "".join(repo_strings)
+        repo_description = REPOS[repo][1]
+        repo_link = UQCS_REPO_URL + REPOS[repo][0]
+        embed.add_field(name=repo, value=f"[{repo_description}]({repo_link})")
 
     repo_group = app_commands.Group(name="repo", description="Commands for UQCS repos")
 
     @repo_group.command(name="list")
     async def repo_list(self, interaction: discord.Interaction):
         """Lists the UQCS GitHub repositories"""
-        await interaction.response.send_message(
-            "_Useful :uqcs: Github repositories_:\n"
-            + self.format_repo_message(list(REPOS.keys()))
+        embed = discord.Embed(
+            title=f"Useful {discord.utils.get(self.bot.emojis, name=UQCS_EMOJI_NAME)} Github Repositories",
+            url=UQCS_REPO_URL,
         )
+        for repo in REPOS.keys():
+            self.add_repo_to_embed(embed, repo)
+        await interaction.response.send_message(embed=embed)
 
     @repo_group.command(name="find", description="Name of the repo to find")
-    async def repo_find(self, interaction: discord.Interaction, name: str):
+    async def repo_find(self, interaction: discord.Interaction, repo: RepoNameType):
         """Finds a specific UQCS GitHub repository"""
-        await interaction.response.send_message(
-            "_Requested UQCS Github repository_:\n" + self.find_repo(name)
+        embed = discord.Embed(
+            title=f"Useful UQCS{discord.utils.get(self.bot.emojis, name=UQCS_EMOJI_NAME)} Github Repositories",
+            url=UQCS_REPO_URL,
         )
-
-    @repo_find.autocomplete("name")
-    async def repo_search_autocomplete(
-        self, interaction: discord.Interaction, current: str
-    ) -> List[app_commands.Choice[str]]:
-        """Autocomplete handler for repo_find command"""
-        repo_names = REPOS.keys()
-        return [
-            app_commands.Choice(name=name, value=name)
-            for name in repo_names
-            if current.lower() in name
-        ]
+        self.add_repo_to_embed(embed, repo)
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot):
