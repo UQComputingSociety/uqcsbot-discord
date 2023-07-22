2b1bea5464e61d5afea883d0ea8724951c7a4610
Isaac Beh <isaacwbeh@gmail.com>
2023-07-22 23:30:54 +1000

    LaTeX: Added error checking for the equation

--- uqcsbot/latex.py
+++ uqcsbot/latex.py
@@ -1,4 +1,5 @@
 from urllib.parse import quote
+import requests
 
 import discord
 from discord import app_commands
@@ -20,6 +21,16 @@ class Latex(commands.Cog):
             "%5Cdpi%7B200%7D%5Cbg%7B36393f%7D%5Cfg%7Bwhite%7D"
             f"{quote(input)}"
         )
+
+        # Check that the image can be found, otherwise it is likely that the equation is invalid
+        status_code = requests.get(url).status_code
+        if status_code == requests.codes.bad_request:
+            await interaction.edit_original_response(content=f"Invalid equation: {input}")
+            return
+        elif status_code != requests.codes.ok:
+            await interaction.edit_original_response(content=f"Could not reach CodeCogs to render LaTeX")
+            return
+        
         embed = discord.Embed(
             colour=discord.Colour.blue(),
             title=f'Latex render for "{input}"',
