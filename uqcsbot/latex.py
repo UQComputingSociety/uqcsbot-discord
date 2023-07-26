42ac75a239cc48493091cce595e0b84871005c97
Isaac Beh <isaacwbeh@gmail.com>
2023-07-26 23:37:53 +1000

    LaTeX: Added error checking for the equation (#141)
    * LaTeX: Added error checking for the equation
    
    * LaTeX: Fix formatting
    
    * Fixed LaTeX embed title being too long
    
    * Fixed formatting
    
    ---------
    
    Co-authored-by: Andrew Brown <92134285+andrewj-brown@users.noreply.github.com>

--- uqcsbot/latex.py
+++ uqcsbot/latex.py
@@ -1,4 +1,5 @@
 from urllib.parse import quote
+import requests
 
 import discord
 from discord import app_commands
@@ -20,9 +21,29 @@ class Latex(commands.Cog):
             "%5Cdpi%7B200%7D%5Cbg%7B36393f%7D%5Cfg%7Bwhite%7D"
             f"{quote(input)}"
         )
+
+        # Check that the image can be found, otherwise it is likely that the equation is invalid
+        status_code = requests.get(url).status_code
+        if status_code == requests.codes.bad_request:
+            await interaction.edit_original_response(
+                content=f"Invalid equation: {input}"
+            )
+            return
+        elif status_code != requests.codes.ok:
+            await interaction.edit_original_response(
+                content=f"Could not reach CodeCogs to render LaTeX"
+            )
+            return
+
+        # Will error if embed title is greater than 256 characters
+        if len(input) >= 256 - len('LaTeX render for ""...'):
+            title = f'LaTeX render for "{input[:220]}..."'
+        else:
+            title = f'LaTeX render for "{input}"'
+
         embed = discord.Embed(
             colour=discord.Colour.blue(),
-            title=f'Latex render for "{input}"',
+            title=title,
         ).set_image(url=f"{url}")
 
         await interaction.edit_original_response(embed=embed)
