b222be09a7fb28a46b4e1ebe55f47dbb87783abb
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2023-10-07 10:08:35 +1000

    Update text.py

--- uqcsbot/text.py
+++ uqcsbot/text.py
@@ -274,5 +274,11 @@ class Text(commands.Cog):
         await interaction.response.send_message(self.zalgo_common(text))
 
 
+    @commands.Cog.listener("on_message")
+    async def cato(self, message):
+        if message.author.id == 344362666258726912:
+            await message.reply("_Ceterum autem censeo praeses capillum esse delendam_.")
+
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(Text(bot))
