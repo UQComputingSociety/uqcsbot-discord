85116b28ceb3c29fcbda3000f3352f37f265d77b
Isaac Beh <isaacwbeh@gmail.com>
2024-03-07 08:46:55 +1000

    Added typing to command_utils.py

--- uqcsbot/utils/command_utils.py
+++ uqcsbot/utils/command_utils.py
@@ -1,7 +1,8 @@
 from random import choice
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
 from discord.ext import commands
+from uqcsbot.bot import UQCSBot
 
 LOADING_REACTS = ["⏰", "🕰️", "⏲️", "🕖", "🕔", "🕥"]
 HYPE_REACTS = [
@@ -16,12 +17,13 @@ HYPE_REACTS = [
 ]
 
 
-def loading_status(command_fn: Callable):
+def loading_status(command_fn: Callable[..., Any]):
     @wraps(command_fn)  # Important to preserve name because `command` uses it
-    async def wrapper(self, ctx: commands.Context, *args):
-        if ctx.message is None or ctx.bot is None:
+    # The use of Any in the following seems unavoidable. Using Any for *args is reasonable, as we want any type of arguments to be passed through. For self, it seems like the cleanest option at the moment: https://stackoverflow.com/a/69528375
+    async def wrapper(self: Any, ctx: commands.Context[UQCSBot], *args: Any):
+        # Check for the bot user before reacting to ensure that a loading react is not left on a message
+        if ctx.bot.user is None:
             return
-
         react = choice(LOADING_REACTS)
         await ctx.message.add_reaction(react)
         res = await command_fn(self, ctx, *args)
