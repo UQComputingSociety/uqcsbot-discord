49eb5eb4e6188a1d708beb353f759e250cc5c3f9
Isaac Beh <isaacwbeh@gmail.com>
2024-03-07 22:37:09 +1000

    Fixed stub issue for error_handler

--- uqcsbot/error_handler.py
+++ uqcsbot/error_handler.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-from discord.ext.commands.errors import MissingRequiredArgument
 import logging
 from typing import Any
 from uqcsbot.bot import UQCSBot
@@ -12,7 +11,7 @@ TODO: this is bundled with advent.py and should be removed.
 class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context[UQCSBot], err: Any):
-        if isinstance(err, MissingRequiredArgument):
+        if isinstance(err, commands.errors.MissingRequiredArgument):
             await ctx.send(
                 "Missing required argument. For further information refer to `!help`"
             )
